f45650705692477f9342a07d8d5589a65f67e4e4
Guilherme Strutzki <guilhermestrutzk@hotmail.com>
2021-05-27 20:25:31 -0300

    Add a car table using stack view

--- class-objects/class-objects/View/ViewController.swift
+++ class-objects/class-objects/View/ViewController.swift
@@ -8,74 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+    @IBOutlet weak var marcaLabel: UILabel!
+    @IBOutlet weak var modeloLabel: UILabel!
+    @IBOutlet weak var anoLabel: UILabel!
+    @IBOutlet weak var motorLabel: UILabel!
+    @IBOutlet weak var combustivelLabel: UILabel!
+    @IBOutlet weak var qtdRodasLabel: UILabel!
+    @IBOutlet weak var cambioAutomaticoLabel: UILabel!
+    @IBOutlet weak var qtdPortasLabel: UILabel!
+    @IBOutlet weak var corLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var arrayCarros:[Carro] = []
-        
-        arrayCarros.append(
-            Carro(
-                qtdRodas: 4,
-                cor: "Azul",
-                qtdPortas: 4,
-                marca: "GM",
-                motor: 2.0,
-                combustivel: ["Gasolina"],
-                ano: 2010, modelo: "Celta",
-                cambioAutomatico: false
-            )
+        let carro: Carro = Carro(
+            qtdRodas: 4,
+            cor: "Preto",
+            qtdPortas: 4,
+            marca: "Audi",
+            motor: 1.8,
+            combustivel: ["Gasolina"],
+            ano: 2018,
+            modelo: "A4",
+            cambioAutomatico: true
         )
         
-        arrayCarros.append(
-            Carro(
-                qtdRodas: 4,
-                cor: "Vermelho",
-                qtdPortas: 4,
-                marca: "VW",
-                motor: 1.0,
-                combustivel: ["Gasolina"],
-                ano: 2020,
-                modelo: "Gol",
-                cambioAutomatico: true
-            )
-        )
-        
-        arrayCarros.append(
-            Carro(
-                qtdRodas: 4,
-                cor: "Preto",
-                qtdPortas: 4,
-                marca: "Audi",
-                motor: 1.8,
-                combustivel: ["Gasolina"],
-                ano: 2018,
-                modelo: "A4",
-                cambioAutomatico: true
-            )
-        )
-        
-        arrayCarros.append(
-            Carro(
-                qtdRodas: 4,
-                cor: "Branco",
-                qtdPortas: 4,
-                marca: "Ford",
-                motor: 2.5,
-                combustivel: ["Diesel"],
-                ano: 2019,
-                modelo: "Ranger",
-                cambioAutomatico: true
-            )
-        )
-       
-        
-        for eachCar in arrayCarros {
-            eachCar.imprimirCarro()
-        }
+        self.marcaLabel.text = "\(carro.marca ?? "")"
+        self.modeloLabel.text = "\(carro.modelo ?? "")"
+        self.anoLabel.text = "\(carro.ano ?? 0)"
+        self.motorLabel.text = "\(carro.motor ?? 0)"
+        self.combustivelLabel.text = "\(carro.combustivel ?? [])"
+        self.qtdRodasLabel.text = "\(carro.qtdRodas ?? 4)"
+        self.cambioAutomaticoLabel.text = "\(carro.cambioAutomatico ?? false)"
+        self.qtdPortasLabel.text = "\(carro.qtdPortas ?? 4)"
+        self.corLabel.text = "\(carro.cor ?? "")"
         
     }
-
-
 }
 
