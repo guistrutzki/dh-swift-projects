42bf0bdc6db4602d105425af20cdabe17016565c
Guilherme Strutzki <guilhermestrutzk@hotmail.com>
2021-06-15 19:42:17 -0300

    Add missing code

---
+++ encapsulamento/encapsulamento/Protocol/DirigivelProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  DirigivelProtocol.swift
+//  encapsulamento
+//
+//  Created by Fernando Douglas Vieira on 10/06/2021.
+//
+
+import Foundation
+
+protocol Dirigivel {
+    func acelerar()
+    func freiar()
+}
